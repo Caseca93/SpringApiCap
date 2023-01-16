af4712ae2e84bd7fc8e99c58815a3e299795b9eb
Caseca93 <icaseca93@gmail.com>
2023-01-16 11:27:15 -0300

    apimovie - usando QuerryMethod

--- src/main/java/org/github/caseca/CapApplication.java
+++ src/main/java/org/github/caseca/CapApplication.java
@@ -1,53 +1,11 @@
 package org.github.caseca;
 
-import org.github.caseca.domain.entity.Movie;
-import org.github.caseca.repository.MovieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class CapApplication {
-    //TODO USAR O SPRING DATA FACILITA MAIS AINDA
-    @Bean
-    public CommandLineRunner init(@Autowired MovieRepository movieRepository){
-        return args -> {
-            System.out.println("Salvando filme");
-            movieRepository.save(new Movie("Thor",
-                                     "www.thor.com",
-                                        "Ficção",
-                            "Amor e Trovão"));
-            movieRepository.save(new Movie("Harry Potter",
-                                    "www.HarryPotter.com",
-                                        "Ficção",
-                            "Camera Secreta"));
 
-            List<Movie> allMovies = movieRepository.findAll();
-            allMovies.forEach(System.out::println);
-
-            System.out.println("Atualizando Filme");
-            allMovies.forEach( c -> {
-                c.setGender(c.getGender() + " atualizou.");
-                movieRepository.update(c);
-            });
-
-            List<Movie> allMoviess = movieRepository.findAll();
-            allMovies.forEach(System.out::println);
-
-            System.out.println("Deletando filme");
-            movieRepository.findAll().forEach(c -> {
-                movieRepository.remove(c.getId());
-            });
-
-        };
-    }
     public static void main(String[] args) {
         SpringApplication.run(CapApplication.class, args);
     }
--- src/main/java/org/github/caseca/repository/MovieRepository.java
+++ src/main/java/org/github/caseca/repository/MovieRepository.java
@@ -1,41 +1,13 @@
 package org.github.caseca.repository;
 
 import org.github.caseca.domain.entity.Movie;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository
-public class MovieRepository {
-
-    @Autowired
-    private EntityManager entityManager;
-
-    @Transactional
-    public Movie save(Movie movie){
-        entityManager.persist(movie);
-        return movie;
-    }
-
-    @Transactional
-    public Movie update(Movie movie){
-        entityManager.merge(movie);
-        return movie;
-    }
-
-    @Transactional
-    public void remove(Long id){
-        Movie movie = entityManager.find( Movie.class, id );
-        entityManager.remove(movie);
-    }
-
-    @Transactional
-    public List<Movie> findAll(){
-        return entityManager
-                .createQuery("from Movie", Movie.class)
-                .getResultList();
-    }
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Query(value = "select m from Movie m where m.name like :name")
+    List<Movie> encontrarPorNome(@Param("name") String name);
 }
