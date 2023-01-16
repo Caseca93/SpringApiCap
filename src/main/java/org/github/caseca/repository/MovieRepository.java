a371cf013e160cc6eb58c0dbb5664e6ca08c4604
Caseca93 <icaseca93@gmail.com>
2023-01-16 10:17:40 -0300

    apimovie - feito o CRUD.

--- src/main/java/org/github/caseca/repository/MovieRepository.java
+++ src/main/java/org/github/caseca/repository/MovieRepository.java
@@ -1,8 +1,41 @@
 package org.github.caseca.repository;
 
+import org.github.caseca.domain.entity.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 public class MovieRepository {
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
+    public Movie save(Movie movie){
+        entityManager.persist(movie);
+        return movie;
+    }
+
+    @Transactional
+    public Movie update(Movie movie){
+        entityManager.merge(movie);
+        return movie;
+    }
+
+    @Transactional
+    public void remove(Long id){
+        Movie movie = entityManager.find( Movie.class, id );
+        entityManager.remove(movie);
+    }
+
+    @Transactional
+    public List<Movie> findAll(){
+        return entityManager
+                .createQuery("from Movie", Movie.class)
+                .getResultList();
+    }
 }
--- src/main/java/org/github/caseca/service/MovieService.java
+++ src/main/java/org/github/caseca/service/MovieService.java
@@ -1,7 +1,12 @@
 package org.github.caseca.service;
 
+import org.github.caseca.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
 }
